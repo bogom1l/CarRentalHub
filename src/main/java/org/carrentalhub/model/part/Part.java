074234b064bf1e97fe38092b7434920e7acd4e04
bogom1l <bogomil.stoev01@gmail.com>
2023-09-22 11:58:37 +0300

    Implemented Part class

--- src/main/java/org/carrentalhub/model/part/Part.java
+++ src/main/java/org/carrentalhub/model/part/Part.java
@@ -1,5 +1,47 @@
 package org.carrentalhub.model.part;
 
 class Part {
-    //TODO
+    private String name;
+    private PartCategory category;
+    private String Description;
+    private double price;
+
+    public Part(String name, PartCategory category, String description, double price) {
+        this.name = name;
+        this.category = category;
+        Description = description;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public PartCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(PartCategory category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
---
+++ src/main/java/org/carrentalhub/model/part/PartCategory.java
@@ -0,0 +1,8 @@
+package org.carrentalhub.model.part;
+
+public enum PartCategory {
+    Engine,
+    Transmission,
+    Interior,
+    Exterior
+}
